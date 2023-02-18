827fb4e3abc466360495467568b0c15739d8c87f
devome <evinedeng@hotmail.com>
2023-02-19 06:47:07 +0800

    01c995b6edd7ffa108d66169f53eda0a86097e2f

--- resource/schemas/TNode/details.js
+++ resource/schemas/TNode/details.js
@@ -6,43 +6,24 @@
       // 设置当前页面
       PTService.pageApp = this;
     }
-    /**
-     * 初始化按钮列表
-     */
+    // 初始化按钮列表
     initButtons() {
       this.initDetailButtons();
     }
-
-    /**
-     * 获取下载链接
-     */
+    // 获取下载链接
     getDownloadURL() {
-      let url = PTService.getFieldValue("downloadURL") || $("a[href*='/api/torrent/download/']").attr('href');
+      let url = PTService.getFieldValue("downloadURL") || $("a[href*='/api/torrent/download/']:contains('复制')").attr('href') || $("a[href*='/api/torrent/download/']").attr('href'); 
       if (!url) {
         return "";
       }
-      // 如果链接地址中不包含passkey，且站点已配置 passkey 信息
-      // 则尝试 passkey 来生成下载链接
-      if (PTService.site.passkey && !(url + "").includes(PTService.site.passkey)) {
-        if (url) {
-          url = url + (url.endsWith('/') ? '' : '/') + PTService.site.passkey
-        }
-      }
-
       return this.getFullURL(url);
     }
-
-    /**
-     * 获取当前种子标题
-     */
+    // 获取当前种子标题
     getTitle() {
       let title = $('label[for="form_item_subtitle"]').parent().next().text() || $('label[for="form_item_title"]').parent().next().text() || '';
-      return subtitle || title;
+      return title;
     }
-    
-    /**
-     * 获取当前种子IMDb Id
-     */
+    // 获取当前种子IMDb Id
     getIMDbId() {
       // TODO
       return null;
